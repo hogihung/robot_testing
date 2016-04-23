e4ceccf473019ba0d6da7c84d4fc9a5964e09547
John Hogarty <hogihung@gmail.com>
2016-04-23 12:24:47 -0400

    Add the robot class with supporting tests.

---
+++ robot.rb
@@ -0,0 +1,8 @@
+class Robot
+  attr_reader :name, :model
+
+  def initialize(name, model)
+    @name = name
+    @model = model
+  end
+end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,3 +1,4 @@
+require './robot'
 
 describe "A robot double" do
   it "returns canned response from the methods named in the provided hash" do
@@ -6,3 +7,19 @@ describe "A robot double" do
     expect(tobor.model).to eq('asiv_mark_two')
   end
 end
+
+describe "Creating an instance of a robot" do
+  it "returns an insance of a Robot" do
+    tobor = Robot.new('Twiki', 'ambuquad')
+    expect(tobor).to be_a Robot
+  end
+end
+
+describe "Creating a robot with name and model" do
+  it "returns a valid robot with name and model" do
+    tobor = Robot.new('Twiki', 'ambuquad')
+    expect(tobor.name).to eq('Twiki')
+    expect(tobor.model).to eq('ambuquad')
+  end
+end
+
